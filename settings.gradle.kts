112ae79953b2d35ab1a6134df335c7499438e9b8
GitHub Action <action@github.com>
2023-01-09 06:48:38 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "tian-intellij-plugin-demo"
--- src/main/kotlin/com/github/tianyu1412/tianintellijplugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tianyu1412/tianintellijplugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tianyu1412.tianintellijplugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tianyu1412.tianintellijplugindemo.MyBundle
 
 class MyApplicationService {
 
